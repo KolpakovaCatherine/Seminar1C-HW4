86f6e178e4827ae4d48b98343360ac396c6ae7f1
Ekaterina Kolpakova <kolpakovapost@yandex.ru>
2022-12-06 09:36:48 +0300

    Ex1 resolved

---
+++ Ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Без использования функции Math.Pow.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+int a = ReadInt("Введите число А: ");
+int b = ReadInt("Введите число B: ");
+
+
+int multiplication = 1;
+
+for (int i = 1; i <= b; i++)
+{
+    multiplication *= a;
+}
+
+Console.WriteLine($"A в степени B = {multiplication}");;
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
